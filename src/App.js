2ac2974bc51638284af1dbcf02eae501f9a47f34
djbalabis <110417718+djbalabis@users.noreply.github.com>
2023-01-26 19:48:04 -0500

    Linked server to client

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const client = new ApolloClient({
+  uri: "https://whatever-marketplace-server.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
