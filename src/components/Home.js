453e43c7f2205057397bb3d39777850fec41135d
Sheev <ericmarcypro@gmail.com>
2023-01-31 21:21:03 -0500

    added components folder and touched relevant files

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return (
+    <div id="main">
+      <main>
+         {/* logo image */}
+        <img src="" alt="" />
+      </main>
+    </div>
+  )
+};
---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+      <div id="navbar">
+        <head>
+            <title> Marketplace </title>
+            <link rel="stylesheet" href="" />
+            <meta />
+        </head>
+
+        <nav>
+            <ul>
+                <li> Home </li>
+                <li> Login </li>
+                <li> Categories </li>
+                <li> Inventory </li>
+                <li> Log Out </li>
+            </ul>
+        </nav>
+      </div>
+    )
+  };
+  
