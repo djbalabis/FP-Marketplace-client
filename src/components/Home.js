629b5a3d0a76424c0cb910f669cea25f923ccbea
djbalabis <110417718+djbalabis@users.noreply.github.com>
2023-02-02 21:36:52 -0500

    Added icons for categories

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
+import { BiMoney } from "react-icons/bi";
+import { GoTools } from "react-icons/go";
+import { MdSportsEsports } from "react-icons/md";
+import { GiConverseShoe, GiWeightLiftingUp, GiBoombox } from "react-icons/gi";
+import { MdOutlineChair } from "react-icons/md";
+
 export const Home = () => {
   return (
     <div id="home">
       <main>
         <h1>home</h1>
-         {/* logo image */}
+        <BiMoney />
+        <GoTools />
+        < MdSportsEsports />
+        <GiConverseShoe />
+        <MdOutlineChair />
+        <GiWeightLiftingUp />
+        <GiBoombox />
         <img src="" alt="" />
       </main>
     </div>
